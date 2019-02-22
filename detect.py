d723b980f9dd0d20714b214bd7fe8044991a5879
Manish baghel <baghelmanish660@gmail.com>
2019-02-22 19:49:46 +0530

    all models updated KCF gives optimal result

--- detect.py
+++ detect.py
@@ -1,74 +1,114 @@
-import cv2, sys, os
-
-if  not (os.path.isfile('goturn.caffemodel') and os.path.isfile('goturn.prototxt')):
-    errorMsg = '''
-    Could not find GOTURN model in current directory.
-    Please ensure goturn.caffemodel and goturn.prototxt are in the current directory
-    '''
-    print(errorMsg)
-    sys.exit()
-
-
-tracker = cv2.TrackerGOTURN_create()   
-video = cv2.VideoCapture("qr2.mp4")
+import cv2
+import sys
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
  
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
 
-if not video.isOpened():
-    print("Could not open video")
-    sys.exit()
+bboxs = []
  
-
-ok,frame = video.read()
-if not ok:
-    print("Cannot read video file")
-    sys.exit()
-
-im = frame
-r = cv2.selectROI(im)
-imCrop = im[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
-cv2.imshow("Image", imCrop)
-cv2.waitKey(0)
-
-
-bbox=r
-print(bbox)
-# Initialize tracker with first frame and bounding box
-ok = tracker.init(frame,bbox)
-print(ok)
-while True:
-    # Read a new frame
+if __name__ == '__main__' :
+ 
+    # Set up tracker.
+    # Instead of MIL, you can also use
+ 
+    tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+    tracker_type = tracker_types[2]
+ 
+    if int(minor_ver) < -1:
+        tracker = cv2.Tracker_create(tracker_type)
+    else:
+        if tracker_type == 'BOOSTING':
+            tracker = cv2.TrackerBoosting_create()
+        if tracker_type == 'MIL':
+            tracker = cv2.TrackerMIL_create()
+        if tracker_type == 'KCF':
+            tracker = cv2.TrackerKCF_create()
+        if tracker_type == 'TLD':
+            tracker = cv2.TrackerTLD_create()
+        if tracker_type == 'MEDIANFLOW':
+            tracker = cv2.TrackerMedianFlow_create()
+        if tracker_type == 'GOTURN':
+            tracker = cv2.TrackerGOTURN_create()
+        if tracker_type == 'MOSSE':
+            tracker = cv2.TrackerMOSSE_create()
+        if tracker_type == "CSRT":
+            tracker = cv2.TrackerCSRT_create()
+ 
+    # Read video
+    video = cv2.VideoCapture("car.mp4")
+ 
+    # Exit if video not opened.
+    if not video.isOpened():
+        print("Could not open video")
+        sys.exit()
+ 
+    # Read first frame.
     ok, frame = video.read()
     if not ok:
-        break
-    timer = cv2.getTickCount()
+        print('Cannot read video file')
+        sys.exit()
+     
+    im = frame
+    r = cv2.selectROI(im)
+    imCrop = im[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
+    cv2.imshow("Image", imCrop)
+    cv2.waitKey(0)
+    bbox = r
  
-    # Update tracker
-    ok, bbox = tracker.update(frame)
-    print(bbox)
+    # Initialize tracker with first frame and bounding box
+    ok = tracker.init(frame, bbox)
  
-    # Calculate Frames per second (FPS)
-    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+    while True:
+        # Read a new frame
+        ok, frame = video.read()
+        if not ok:
+            break
+         
+        # Start timer
+        timer = cv2.getTickCount()
  
-    # Draw bounding box
-    if ok:
-        # Tracking success
-        p1 = (int(bbox[0]+200), int(bbox[1]))
-        p2 = (int(bbox[0]+ 400+ bbox[2]), int(bbox[1] + bbox[3]))
-        cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
-    else :
-        # Tracking failure
-        cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+        # Update tracker
+        ok, bbox = tracker.update(frame)
  
-    # Display tracker type on frame
-    cv2.putText(frame, "GOTURN Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
+        # Calculate Frames per second (FPS)
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
  
-    # Display FPS on frame
-    cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+        # Draw bounding box
+        if ok:
+            # Tracking success
+            # print(bbox)
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+            cv2.circle(frame, (int(bbox[0] + bbox[2]/2), int(bbox[1] + bbox[3]/2)), 3, (0, 255, 0), -1)
+            point = (int(bbox[0] + bbox[2]/2), int(bbox[1] + bbox[3]/2))
+            bboxs.append(point)
+        else :
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
  
-    # Display result
-    cv2.imshow("Tracking", frame)
-  
-    # Exit if ESC pressed
-    k = cv2.waitKey(1) & 0xff
-    if k == 27:
-        break
+        # Display tracker type on frame
+        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
+     
+        # Display FPS on frame
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+ 
+        # Display result
+        cv2.imshow("Tracking", frame)
+ 
+        # Exit if ESC pressed
+        k = cv2.waitKey(1) & 0xff
+        if k == 27 : break
+
+    # print(bboxs)
+
+    fig,ax = plt.subplots()
+    bboxs = pd.DataFrame(bboxs)
+    x = bboxs.iloc[:,0]
+    y = bboxs.index.values
+
+    ax.plot(x,y)
+    fig.suptitle("Some Random Bullshit")
+    plt.show()
